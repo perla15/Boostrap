e1fcd4da1cb58de6c0368135a71f0e5b722402f5
perla15 <70298505+perla15@users.noreply.github.com>
2020-11-27 00:10:41 -0700

    Update config.php

--- config.php
+++ config.php
@@ -8,6 +8,9 @@
 	if (!isset($_SESSION)) {
 		session_start();
 	}
+	if(!isset($_SESSION['token'])) {
+		$_SESSION['token'] = md5(uniqid(mt_rand(),true));
+	}
 	if (!defined('BASE_PATH')) {
 		define('BASE_PATH',"http://localhost/progra_avanzada/boostrap/");
 	}
